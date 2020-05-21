cbed4d2fe30062702541022279bba33411696065
Pramod Thomas <pmod23@gmail.com>
2020-05-21 13:48:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,12 +13,11 @@ end
 
 def selection(num)
   if num = 1
-    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-  binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
+  binding.pry
 end
 
 def runner
